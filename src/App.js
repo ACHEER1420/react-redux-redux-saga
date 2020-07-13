bb7d77a143e95b0ee978112bb06691f1b424024c
ACHEER <15667217+ACHEER1420@users.noreply.github.com>
2020-07-13 14:58:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles.css";
+import Homepage from "./pages/homepage/homepage.component";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Homepage />
     </div>
   );
 }
